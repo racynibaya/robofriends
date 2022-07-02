47348df59dd91a41b8fcc2b37afe2a0209f0d3db
Racyn Ibaya <racynibaya@outlook.com>
2022-07-02 18:24:21 -0400

    configure the car

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './Card.css';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='card tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=280x280`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
